d0e5c4a3c84936997ef14946f070c7eee1873e8c
Lucas Ferreira de Vasconcelos Ribeiro <144264783+Lukitas20-beep@users.noreply.github.com>
2025-03-25 20:22:05 -0300

    Atualizar o ProgramaNumeroComplexo.java

--- ProgramaNumeroComplexo.java
+++ ProgramaNumeroComplexo.java
@@ -1,18 +1,17 @@
 public class ProgramaNumeroComplexo {
     public static void main(String[] args) {
-        // Criando dois números complexos
-        NumeroComplexo z1 = new NumeroComplexo(2, 3); // 2 + 3i
-        NumeroComplexo z2 = new NumeroComplexo(4, 5); // 4 + 5i
+        NumeroComplexo z1 = new NumeroComplexo(2, 3); 
+        NumeroComplexo z2 = new NumeroComplexo(4, 5); 
 
-        // Adicionando os números complexos
+        
         NumeroComplexo resultadoAdicao = z1.adicionar(z2);
         System.out.println("Resultado da adição: " + resultadoAdicao);
 
-        // Subtraindo os números complexos
+        
         NumeroComplexo resultadoSubtracao = z1.subtrair(z2);
         System.out.println("Resultado da subtração: " + resultadoSubtracao);
 
-        // Multiplicando os números complexos
+        
         NumeroComplexo resultadoMultiplicacao = z1.multiplicar(z2);
         System.out.println("Resultado da multiplicação: " + resultadoMultiplicacao);
     }
